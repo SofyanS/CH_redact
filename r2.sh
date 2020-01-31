dd2626869eb11776f8e72f1d2c279b459dab543d
Sofyan Saputra <sofyansaputra@sofyansaputra-macbookpro.roam.corp.google.com>
2020-01-31 02:25:47 -0500

    File upload should be after cloud function created

--- r2.sh
+++ r2.sh
@@ -1,2 +1,6 @@
 # Create cloud function to process audio filess when uploaded to the bucket
 yes | gcloud functions deploy safLongRunJobFunc --runtime nodejs8 --trigger-resource gs://$DEVSHELL_PROJECT_ID-upload --trigger-event google.storage.object.finalize --source=saf-longrun-job-func
+
+
+# Upload Sample Audio Files for Processing
+gsutil -h x-goog-meta-agentid:1234567 -h x-goog-meta-stereo:true -h x-goog-meta-agentchannel:1 -h x-goog-meta-pubsubtopicname:payload -h x-goog-meta-year:2019 -h x-goog-meta-month:11 -h x-goog-meta-day:06 -h x-goog-meta-starttime:1116 -m cp sample-audio/transcript1.wav gs://$DEVSHELL_PROJECT_ID-upload/
--- saf-longrun-job-dataflow/r3.sh
+++ saf-longrun-job-dataflow/r3.sh
@@ -2,6 +2,3 @@
 pip install -r requirements.txt
 python saflongrunjobdataflow.py --output_bigquery $DEVSHELL_PROJECT_ID:saf.transcripts --input_topic projects/$DEVSHELL_PROJECT_ID/topics/payload --region us-central1 --project $DEVSHELL_PROJECT_ID --temp_location gs://$DEVSHELL_PROJECT_ID-staging/ --staging_location gs://$DEVSHELL_PROJECT_ID-staging/ --save_main_session True --runner DataflowRunner
 cd ..
-
-# Upload Sample Audio Files for Processing
-gsutil -h x-goog-meta-agentid:1234567 -h x-goog-meta-stereo:true -h x-goog-meta-agentchannel:1 -h x-goog-meta-pubsubtopicname:payload -h x-goog-meta-year:2019 -h x-goog-meta-month:11 -h x-goog-meta-day:06 -h x-goog-meta-starttime:1116 -m cp sample-audio/transcript1.wav gs://$DEVSHELL_PROJECT_ID-upload/
