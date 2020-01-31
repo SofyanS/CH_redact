f22d664384a2c09f5bed39e1a90eb2abc196f628
Sofyan Saputra <sofyansaputra@sofyansaputra-macbookpro.roam.corp.google.com>
2020-01-31 00:02:08 -0500

    Fix

--- redacting.sh
+++ redacting.sh
@@ -17,16 +17,17 @@ bq mk --table $DEVSHELL_PROJECT_ID:saf.transcripts bigquery/schema.json
 yes | gcloud functions deploy safLongRunJobFunc --runtime nodejs8 --trigger-resource gs://$DEVSHELL_PROJECT_ID-upload --trigger-event google.storage.object.finalize --source=saf-longrun-job-func
 
 # Deploy a Cloud Dataflow Pipeline (how?)
-#python -m virtualenv env -p python3
-#pip install -r requirements.txt
 cd saf-longrun-job-dataflow
+python -m virtualenv env -p python3
 source env/bin/activate
-python saflongrunjobdataflow.py --output_bigquery=$DEVSHELL_PROJECT_ID:saf.transcripts --input_topic=projects/$DEVSHELL_PROJECT_ID/topics/payload --region=us-central1 --project=$DEVSHELL_PROJECT_ID --temp_location=gs://$DEVSHELL_PROJECT_ID-staging/ --staging_location=gs://$DEVSHELL_PROJECT_ID-staging/ --runner=DataflowRunner
+pip install -r requirements.txt
+python saflongrunjobdataflow.py --output_bigquery $DEVSHELL_PROJECT_ID:saf.transcripts --input_topic projects/$DEVSHELL_PROJECT_ID/topics/payload --region us-central1 --project $DEVSHELL_PROJECT_ID --temp_location gs://$DEVSHELL_PROJECT_ID-staging/ --staging_location gs://$DEVSHELL_PROJECT_ID-staging/ --requirements_file requirements.txt --runner DataflowRunner
 cd ..
+# --save_main_session True 
 
 # Upload Sample Audio Files for Processing
-gsutil -h x-goog-meta-agentid:1234567 -h x-goog-meta-stereo:true -h x-goog-meta-agentchannel:1 -h x-goog-meta-pubsubtopicname:processAudio -h x-goog-meta-year:2019 -h x-goog-meta-month:11 -h x-goog-meta-day:06 -h x-goog-meta-starttime:1116 cp  * gs://$DEVSHELL_PROJECT_ID-upload/
-
+gsutil -h x-goog-meta-agentid:1234567 -h x-goog-meta-stereo:true -h x-goog-meta-agentchannel:1 -h x-goog-meta-pubsubtopicname:payload -h x-goog-meta-year:2019 -h x-goog-meta-month:11 -h x-goog-meta-day:06 -h x-goog-meta-starttime:1116 cp sample-audio/transcript1.wav gs://$DEVSHELL_PROJECT_ID-upload/
+gsutil -h x-goog-meta-agentid:1234567 -h x-goog-meta-stereo:true -h x-goog-meta-agentchannel:1 -h x-goog-meta-pubsubtopicname:payload -h x-goog-meta-year:2019 -h x-goog-meta-month:11 -h x-goog-meta-day:06 -h x-goog-meta-starttime:1116 cp sample-audio/* gs://$DEVSHELL_PROJECT_ID-upload/
 
 # Run a DLP Job https://cloud.google.com/bigquery/docs/scan-with-dlp
 
