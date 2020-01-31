b88b9c8d9c34ce2ff0ee15cfd4477cbd4f5684a8
Sofyan Saputra <sofyansaputra@sofyansaputra-macbookpro.roam.corp.google.com>
2020-01-31 01:14:18 -0500

    just one sample-audio file

--- r3.sh
+++ r3.sh
@@ -1,12 +1,7 @@
 # Deploy a Cloud Dataflow Pipeline
 cd saf-longrun-job-dataflow
 source env/bin/activate
-python saflongrunjobdataflow.py --output_bigquery $DEVSHELL_PROJECT_ID:saf.transcripts --input_topic projects/$DEVSHELL_PROJECT_ID/topics/payload --region us-central1 --project $DEVSHELL_PROJECT_ID --temp_location gs://$DEVSHELL_PROJECT_ID-staging/ --staging_location gs://$DEVSHELL_PROJECT_ID-staging/ --requirements_file requirements.txt --runner DataflowRunner
-cd ..
+python saflongrunjobdataflow.py --output_bigquery $DEVSHELL_PROJECT_ID:saf.transcripts --input_topic projects/$DEVSHELL_PROJECT_ID/topics/payload --region us-central1 --project $DEVSHELL_PROJECT_ID --temp_location gs://$DEVSHELL_PROJECT_ID-staging/ --staging_location gs://$DEVSHELL_PROJECT_ID-staging/ --save_main_session True --runner DataflowRunner
 
 # Upload Sample Audio Files for Processing
-gsutil -h x-goog-meta-agentid:1234567 -h x-goog-meta-stereo:true -h x-goog-meta-agentchannel:1 -h x-goog-meta-pubsubtopicname:payload -h x-goog-meta-year:2019 -h x-goog-meta-month:11 -h x-goog-meta-day:06 -h x-goog-meta-starttime:1116 cp  sample-audio/* gs://$DEVSHELL_PROJECT_ID-upload/
-
-
-# Run a DLP Job https://cloud.google.com/bigquery/docs/scan-with-dlp
-
+gsutil -h x-goog-meta-agentid:1234567 -h x-goog-meta-stereo:true -h x-goog-meta-agentchannel:1 -h x-goog-meta-pubsubtopicname:payload -h x-goog-meta-year:2019 -h x-goog-meta-month:11 -h x-goog-meta-day:06 -h x-goog-meta-starttime:1116 -m cp sample-audio/transcript1 gs://$DEVSHELL_PROJECT_ID-upload/
--- redacting.sh
+++ redacting.sh
@@ -19,14 +19,12 @@ yes | gcloud functions deploy safLongRunJobFunc --runtime nodejs8 --trigger-reso
 # Deploy a Cloud Dataflow Pipeline 
 # --save_main_session True --requirements_file requirements.txt
 cd saf-longrun-job-dataflow
-python -m virtualenv env -p python3
-source env/bin/activate
-sudo pip3 install -r requirements.txt
+#python -m virtualenv env -p python3
+#source env/bin/activate
+#sudo pip3 install -r requirements.txt
 python saflongrunjobdataflow.py --output_bigquery $DEVSHELL_PROJECT_ID:saf.transcripts --input_topic projects/$DEVSHELL_PROJECT_ID/topics/payload --region us-central1 --project $DEVSHELL_PROJECT_ID --temp_location gs://$DEVSHELL_PROJECT_ID-staging/ --staging_location gs://$DEVSHELL_PROJECT_ID-staging/ --save_main_session True --runner DataflowRunner
-cd ..
 
 # Upload Sample Audio Files for Processing
-gsutil -h x-goog-meta-agentid:1234567 -h x-goog-meta-stereo:true -h x-goog-meta-agentchannel:1 -h x-goog-meta-pubsubtopicname:payload -h x-goog-meta-year:2019 -h x-goog-meta-month:11 -h x-goog-meta-day:06 -h x-goog-meta-starttime:1116 cp sample-audio/transcript1.wav gs://$DEVSHELL_PROJECT_ID-upload/
 gsutil -h x-goog-meta-agentid:1234567 -h x-goog-meta-stereo:true -h x-goog-meta-agentchannel:1 -h x-goog-meta-pubsubtopicname:payload -h x-goog-meta-year:2019 -h x-goog-meta-month:11 -h x-goog-meta-day:06 -h x-goog-meta-starttime:1116 cp sample-audio/* gs://$DEVSHELL_PROJECT_ID-upload/
 
 # Run a DLP Job https://cloud.google.com/bigquery/docs/scan-with-dlp
